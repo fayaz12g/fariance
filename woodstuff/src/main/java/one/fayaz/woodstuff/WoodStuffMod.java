c4823e99286a84a51a36704f25e71c68c81f0c21
Fayaz Shaikh <61674751+fayaz12g@users.noreply.github.com>
2024-09-17 18:42:36 -0400

    Update WoodStuffMod.java

--- woodstuff/src/main/java/one/fayaz/woodstuff/WoodStuffMod.java
+++ woodstuff/src/main/java/one/fayaz/woodstuff/WoodStuffMod.java
@@ -5,48 +5,63 @@ import net.minecraft.core.registries.Registries;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.MapColor;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
-import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.config.ModConfig;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.RegistryObject;
 import org.slf4j.Logger;
 
-// The value here should match an entry in the META-INF/mods.toml file
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Mod(WoodStuffMod.MODID)
 public class WoodStuffMod {
     public static final String MODID = "woodstuff";
     private static final Logger LOGGER = LogUtils.getLogger();
 
-    // Create a Deferred Register to hold Items which will all be registered under the "woodstuff" namespace
+    // Create Deferred Registers for Items and CreativeModeTabs
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(Registries.ITEM, MODID);
-    // Create a Deferred Register to hold CreativeModeTabs which will all be registered under the "woodstuff" namespace
     public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MODID);
 
-    // Register your items
+    // Register the items from ItemRegistry
     public static final ItemRegistry ItemRegistry = new ItemRegistry();
 
-    // Create a new CreativeModeTab
-    public static final RegistryObject<CreativeModeTab> WOODSTUFF_TAB = CREATIVE_MODE_TABS.register("woodstuff_tab", () -> CreativeModeTab.builder()
+    // Create CreativeModeTabs for each tool type
+    public static final RegistryObject<CreativeModeTab> SWORDS_TAB = CREATIVE_MODE_TABS.register("swords_tab", () -> CreativeModeTab.builder()
             .withTabsBefore(CreativeModeTabs.BUILDING_BLOCKS)
             .icon(() -> ItemRegistry.GENERATED_ITEMS.get("oak_sword_with_oak_stick").get().getDefaultInstance())
-            .displayItems((parameters, output) -> {
-                // Add all items from the generated items to the creative tab
-                ItemRegistry.GENERATED_ITEMS.values().forEach(itemRegistryObject -> output.accept(itemRegistryObject.get()));
-            }).build());
+            .displayItems((parameters, output) -> addItemsToTab(output, "sword"))
+            .build());
+
+    public static final RegistryObject<CreativeModeTab> PICKAXES_TAB = CREATIVE_MODE_TABS.register("pickaxes_tab", () -> CreativeModeTab.builder()
+            .withTabsBefore(CreativeModeTabs.BUILDING_BLOCKS)
+            .icon(() -> ItemRegistry.GENERATED_ITEMS.get("oak_pickaxe_with_oak_stick").get().getDefaultInstance())
+            .displayItems((parameters, output) -> addItemsToTab(output, "pickaxe"))
+            .build());
+
+    public static final RegistryObject<CreativeModeTab> AXES_TAB = CREATIVE_MODE_TABS.register("axes_tab", () -> CreativeModeTab.builder()
+            .withTabsBefore(CreativeModeTabs.BUILDING_BLOCKS)
+            .icon(() -> ItemRegistry.GENERATED_ITEMS.get("oak_axe_with_oak_stick").get().getDefaultInstance())
+            .displayItems((parameters, output) -> addItemsToTab(output, "axe"))
+            .build());
+
+    public static final RegistryObject<CreativeModeTab> SHOVELS_TAB = CREATIVE_MODE_TABS.register("shovels_tab", () -> CreativeModeTab.builder()
+            .withTabsBefore(CreativeModeTabs.BUILDING_BLOCKS)
+            .icon(() -> ItemRegistry.GENERATED_ITEMS.get("oak_shovel_with_oak_stick").get().getDefaultInstance())
+            .displayItems((parameters, output) -> addItemsToTab(output, "shovel"))
+            .build());
+
+    public static final RegistryObject<CreativeModeTab> HOES_TAB = CREATIVE_MODE_TABS.register("hoes_tab", () -> CreativeModeTab.builder()
+            .withTabsBefore(CreativeModeTabs.BUILDING_BLOCKS)
+            .icon(() -> ItemRegistry.GENERATED_ITEMS.get("oak_hoe_with_oak_stick").get().getDefaultInstance())
+            .displayItems((parameters, output) -> addItemsToTab(output, "hoe"))
+            .build());
 
     public WoodStuffMod(FMLJavaModLoadingContext context) {
         IEventBus modEventBus = context.getModEventBus();
@@ -65,5 +80,20 @@ public class WoodStuffMod {
     private void commonSetup(final FMLCommonSetupEvent event) {
         // Some common setup code
         LOGGER.info("HELLO FROM COMMON SETUP");
+    }
+
+    private static void addItemsToTab(CreativeModeTab.Output output, String toolType) {
+        List<RegistryObject<Item>> sortedItems = ItemRegistry.GENERATED_ITEMS.values().stream()
+                .filter(itemRegistryObject -> itemRegistryObject.getId().getPath().contains(toolType))
+                .sorted((o1, o2) -> {
+                    String name1 = o1.getId().getPath();
+                    String name2 = o2.getId().getPath();
+                    return name1.compareTo(name2); // Sort by item name
+                })
+                .collect(Collectors.toList());
+
+        for (RegistryObject<Item> item : sortedItems) {
+            output.accept(item.get());
+        }
     }
 }
