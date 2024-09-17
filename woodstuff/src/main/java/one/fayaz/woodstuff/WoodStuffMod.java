4c0ca0e47757a6afe04376def6c8473f99a6052f
Fayaz Shaikh <61674751+fayaz12g@users.noreply.github.com>
2024-09-17 18:54:42 -0400

    new textures

--- woodstuff/src/main/java/one/fayaz/woodstuff/WoodStuffMod.java
+++ woodstuff/src/main/java/one/fayaz/woodstuff/WoodStuffMod.java
@@ -2,6 +2,7 @@ package one.fayaz.woodstuff;
 
 import com.mojang.logging.LogUtils;
 import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
@@ -34,32 +35,37 @@ public class WoodStuffMod {
 
     // Create CreativeModeTabs for each tool type
     public static final RegistryObject<CreativeModeTab> SWORDS_TAB = CREATIVE_MODE_TABS.register("swords_tab", () -> CreativeModeTab.builder()
-            .withTabsBefore(CreativeModeTabs.BUILDING_BLOCKS)
+            .withTabsAfter(CreativeModeTabs.SEARCH)
             .icon(() -> ItemRegistry.GENERATED_ITEMS.get("oak_sword_with_oak_stick").get().getDefaultInstance())
+            .title(Component.translatable("itemGroup.woodstuff.swords"))
             .displayItems((parameters, output) -> addItemsToTab(output, "sword"))
             .build());
 
     public static final RegistryObject<CreativeModeTab> PICKAXES_TAB = CREATIVE_MODE_TABS.register("pickaxes_tab", () -> CreativeModeTab.builder()
-            .withTabsBefore(CreativeModeTabs.BUILDING_BLOCKS)
+            .withTabsAfter(CreativeModeTabs.SEARCH)
             .icon(() -> ItemRegistry.GENERATED_ITEMS.get("oak_pickaxe_with_oak_stick").get().getDefaultInstance())
+            .title(Component.translatable("itemGroup.woodstuff.pickaxes"))
             .displayItems((parameters, output) -> addItemsToTab(output, "pickaxe"))
             .build());
 
     public static final RegistryObject<CreativeModeTab> AXES_TAB = CREATIVE_MODE_TABS.register("axes_tab", () -> CreativeModeTab.builder()
-            .withTabsBefore(CreativeModeTabs.BUILDING_BLOCKS)
+            .withTabsAfter(CreativeModeTabs.SEARCH)
             .icon(() -> ItemRegistry.GENERATED_ITEMS.get("oak_axe_with_oak_stick").get().getDefaultInstance())
+            .title(Component.translatable("itemGroup.woodstuff.axes"))
             .displayItems((parameters, output) -> addItemsToTab(output, "axe"))
             .build());
 
     public static final RegistryObject<CreativeModeTab> SHOVELS_TAB = CREATIVE_MODE_TABS.register("shovels_tab", () -> CreativeModeTab.builder()
-            .withTabsBefore(CreativeModeTabs.BUILDING_BLOCKS)
+            .withTabsAfter(CreativeModeTabs.SEARCH)
             .icon(() -> ItemRegistry.GENERATED_ITEMS.get("oak_shovel_with_oak_stick").get().getDefaultInstance())
+            .title(Component.translatable("itemGroup.woodstuff.shovels"))
             .displayItems((parameters, output) -> addItemsToTab(output, "shovel"))
             .build());
 
     public static final RegistryObject<CreativeModeTab> HOES_TAB = CREATIVE_MODE_TABS.register("hoes_tab", () -> CreativeModeTab.builder()
-            .withTabsBefore(CreativeModeTabs.BUILDING_BLOCKS)
+            .withTabsAfter(CreativeModeTabs.SEARCH)
             .icon(() -> ItemRegistry.GENERATED_ITEMS.get("oak_hoe_with_oak_stick").get().getDefaultInstance())
+            .title(Component.translatable("itemGroup.woodstuff.hoes"))
             .displayItems((parameters, output) -> addItemsToTab(output, "hoe"))
             .build());
 
