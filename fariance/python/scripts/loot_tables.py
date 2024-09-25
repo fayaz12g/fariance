3cec9a95996174e297815392ef67e4aff6bcedcd
Fayaz Shaikh <61674751+fayaz12g@users.noreply.github.com>
2024-09-25 15:56:25 -0400

    use tools and drop table/furnace

--- fariance/python/scripts/loot_tables.py
+++ fariance/python/scripts/loot_tables.py
@@ -4,8 +4,7 @@ import os
 from PIL import Image, ImageOps
 from constants import *
 
-
-def generate_loot_tables():
+def ladder_loot_tables():
     # Loop through each wood type to create a loot table for each ladder
     for wood in WOOD_TYPES:
         ladder_name = f"{wood}_ladder"
@@ -35,5 +34,96 @@ def generate_loot_tables():
         # Write the loot table data to the file
         with open(loot_table_file_path, 'w') as f:
             json.dump(loot_table_data, f, indent=2)
+
+def table_loot_tables():
+    # Loop through each wood type to create a loot table for each crafting table
+    for wood in WOOD_TYPES:
+        table_name = f"{wood}_crafting_table"
+
+        # Define the loot table structure
+        loot_table_data = {
+            "type": "minecraft:block",
+            "pools": [
+                {
+                "bonus_rolls": 0.0,
+                "conditions": [
+                    {
+                    "condition": "minecraft:survives_explosion"
+                    }
+                ],
+                "entries": [
+                    {
+                    "type": "minecraft:item",
+                    "name": f"fariance:{table_name}"
+                    }
+                ],
+                "rolls": 1.0
+                }
+            ],
+            "random_sequence": f"fariance:blocks/{table_name}"
+        }
+
+        # Build the output file path for each table's loot table
+        loot_table_file_path = os.path.join(output_dir, "data", "fariance", "loot_tables", "blocks", f"{table_name}.json")
+
+        # Ensure the directory exists
+        os.makedirs(os.path.dirname(loot_table_file_path), exist_ok=True)
+
+        # Write the loot table data to the file
+        with open(loot_table_file_path, 'w') as f:
+            json.dump(loot_table_data, f, indent=2)
+
+def furnace_loot_tables():
+    # Loop through each stone type to create a loot table for each furnace
+    for stone in STONE_TYPES:
+        furnace_name = f"{stone}_furnace"
+
+        # Define the loot table structure
+        loot_table_data = {
+            "type": "minecraft:block",
+            "pools": [
+                {
+                "bonus_rolls": 0.0,
+                "conditions": [
+                    {
+                    "condition": "minecraft:survives_explosion"
+                    }
+                ],
+                "entries": [
+                    {
+                    "type": "minecraft:item",
+                    "functions": [
+                        {
+                        "function": "minecraft:copy_components",
+                        "include": [
+                            "minecraft:custom_name"
+                        ],
+                        "source": "block_entity"
+                        }
+                    ],
+                    "name": f"fariance:{furnace_name}"
+                    }
+                ],
+                "rolls": 1.0
+                }
+            ],
+            "random_sequence": f"fariance:blocks/{furnace_name}"
+        }
+
+        # Build the output file path for each furnaces's loot table
+        loot_table_file_path = os.path.join(output_dir, "data", "fariance", "loot_tables", "blocks", f"{furnace_name}.json")
+
+        # Ensure the directory exists
+        os.makedirs(os.path.dirname(loot_table_file_path), exist_ok=True)
+
+        # Write the loot table data to the file
+        with open(loot_table_file_path, 'w') as f:
+            json.dump(loot_table_data, f, indent=2)
+
+
+def generate_loot_tables():
+    ladder_loot_tables()
+    table_loot_tables()
+    furnace_loot_tables()
 
     print(f"Loot tables generated!")
--- fariance/python/scripts/mineable.py
+++ fariance/python/scripts/mineable.py
@@ -4,12 +4,14 @@ import os
 from PIL import Image, ImageOps
 from constants import *
 
-def generate_mineable_json():
+def mineable_axe_json():
     # Define the mineable tag structure
     mineable_data = {
         "replace": False,
         "values": [
-            f"fariance:{wood}_ladder" for wood in WOOD_TYPES  # Add each wood ladder to the values list
+            f"fariance:{wood}_ladder"   # Add each wood ladder to the values list
+            f"fariance:{wood}_crafting_table"   # Add each wood crafting table to the values list
+            for wood in WOOD_TYPES
         ]
     }
 
@@ -22,5 +24,29 @@ def generate_mineable_json():
     # Write the mineable data to the file
     with open(mineable_file_path, 'w') as f:
         json.dump(mineable_data, f, indent=2)
+
+def mineable_pickaxe_json():
+    # Define the mineable tag structure
+    mineable_data = {
+        "replace": False,
+        "values": [
+            f"fariance:{stone}_furnace"   # Add each stone furnace to the values list
+            for stone in STONE_TYPES
+        ]
+    }
+
+    # Build the full output path for the mineable/axe.json file
+    mineable_file_path = os.path.join(output_dir, "data", "minecraft", "tags", "blocks", "mineable", "pickaxe.json")
+
+    # Ensure the directory exists
+    os.makedirs(os.path.dirname(mineable_file_path), exist_ok=True)
+
+    # Write the mineable data to the file
+    with open(mineable_file_path, 'w') as f:
+        json.dump(mineable_data, f, indent=2)
+
+def generate_mineable_json():
+    mineable_axe_json
+    mineable_pickaxe_json()
 
     print(f"Mineable ladders JSON generated")
