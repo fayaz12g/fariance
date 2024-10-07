ca8b88416eeb9a6dc6a8d48ccab398261cf9f31c
Fayaz Shaikh <61674751+fayaz12g@users.noreply.github.com>
2024-10-07 15:18:08 -0400

    custom signs classes

--- fariance/src/main/java/one/fayaz/fariance/ItemRegistry.java
+++ fariance/src/main/java/one/fayaz/fariance/ItemRegistry.java
@@ -290,36 +290,20 @@ public class ItemRegistry {
 
             // Standing Sign
             RegistryObject<Block> sign = BLOCKS.register(woodName + "_sign",
-                    () -> new StandingSignBlock(WoodType.OAK, BlockBehaviour.Properties.of()
-                            .noCollission().strength(1.0F).sound(SoundType.WOOD)));
+                    () -> new CustomStandingSignBlock(BlockBehaviour.Properties.of()
+                            .noCollission().strength(1.0F).sound(SoundType.WOOD), WoodType.OAK));
             GENERATED_BLOCKS.put(woodName + "_sign", sign);
 
             // Wall Sign
             RegistryObject<Block> wallSign = BLOCKS.register(woodName + "_wall_sign",
-                    () -> new WallSignBlock(WoodType.OAK, BlockBehaviour.Properties.of()
-                            .noCollission().strength(1.0F).sound(SoundType.WOOD)));
+                    () -> new CustomWallSignBlock(BlockBehaviour.Properties.of()
+                            .noCollission().strength(1.0F).sound(SoundType.WOOD), WoodType.OAK));
             GENERATED_BLOCKS.put(woodName + "_wall_sign", wallSign);
 
             // Register sign item (for both standing and wall signs)
             GENERATED_ITEMS.put(woodName + "_sign", ITEMS.register(woodName + "_sign",
                     () -> new SignItem(new Item.Properties().stacksTo(16), sign.get(), wallSign.get())));
 
-            // Hanging Sign
-            RegistryObject<Block> hangingSign = BLOCKS.register(woodName + "_hanging_sign",
-                    () -> new CeilingHangingSignBlock(WoodType.OAK, BlockBehaviour.Properties.of()
-                            .noCollission().strength(1.0F).sound(SoundType.WOOD)));
-            GENERATED_BLOCKS.put(woodName + "_hanging_sign", hangingSign);
-
-            // Wall Hanging Sign
-            RegistryObject<Block> wallHangingSign = BLOCKS.register(woodName + "_wall_hanging_sign",
-                    () -> new WallHangingSignBlock(WoodType.OAK, BlockBehaviour.Properties.of()
-                            .noCollission().strength(1.0F).sound(SoundType.WOOD)));
-            GENERATED_BLOCKS.put(woodName + "_wall_hanging_sign", wallHangingSign);
-
-            // Register hanging sign item (for both ceiling and wall hanging signs)
-            GENERATED_ITEMS.put(woodName + "_hanging_sign", ITEMS.register(woodName + "_hanging_sign",
-                    () -> new HangingSignItem(hangingSign.get(), wallHangingSign.get(), new Item.Properties().stacksTo(16))));
-//
 //            // Boat (broken)
 //            if (!NETHER_WOODS.contains(wood)) {
 //                RegistryObject<Item> boat = ITEMS.register(woodName + "_boat",
---
+++ fariance/src/main/java/one/fayaz/fariance/blocks/CustomWallSignBlock.java
@@ -0,0 +1,19 @@
+package one.fayaz.fariance.blocks;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.block.WallSignBlock;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.SignBlockEntity;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.WoodType;
+
+public class CustomWallSignBlock extends WallSignBlock {
+    public CustomWallSignBlock(Properties properties, WoodType woodType) {
+        super(woodType, properties);
+    }
+
+    @Override
+    public BlockEntity newBlockEntity(BlockPos pos, BlockState state) {
+        return new SignBlockEntity(pos, state);
+    }
+}
