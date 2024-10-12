adb88e8e05aa35632f3f8b389a15f4ad8da37ac2
Fayaz Shaikh <61674751+fayaz12g@users.noreply.github.com>
2024-10-11 20:46:23 -0400

    Update FarianceMod.java

--- fariance/src/main/java/one/fayaz/fariance/FarianceMod.java
+++ fariance/src/main/java/one/fayaz/fariance/FarianceMod.java
@@ -168,7 +168,7 @@ public class FarianceMod {
     }
 
     private static void addMiscItemsToTab(CreativeModeTab.Output output) {
-        String[] groupingWords = {"button", "trapdoor", "planks", "stem", "log", "plate", "gate", "fence"};
+        String[] groupingWords = {"button", "trapdoor", "planks", "stem", "log", "plate", "gate", "fence", "slab"};
         Set<String> groupingWordsSet = new HashSet<>(Arrays.asList(groupingWords));
 
         List<RegistryObject<Item>> sortedItems = ItemRegistry.GENERATED_ITEMS.values().stream()
